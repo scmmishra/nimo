94c01a7b0790f892c8ffc8408fd614315b6ee4e0
Shivam Mishra <scm.mymail@gmail.com>
2020-05-02 12:32:58 +0530

    feat: added dayjs and tailwind dark mode

--- static/postcss.config.js
+++ static/postcss.config.js
@@ -5,7 +5,9 @@ module.exports = {
     process.env.NODE_ENV === 'production' && require('@fullhuman/postcss-purgecss')({
       content: [
         './public/*.html',
+        './node_modules/tailwindcss-dark-mode/prefers-dark.js',
       ],
+      whitelist: ['mode-dark'],
       defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
     })
   ]
