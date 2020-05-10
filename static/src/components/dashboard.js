8b16789ec9aa9e98818090b895945bf07e873e6c
Shivam Mishra <scm.mymail@gmail.com>
2020-05-10 21:42:03 +0530

    feat: pass filters to dashboard api

--- static/src/components/dashboard.js
+++ static/src/components/dashboard.js
@@ -1,4 +1,4 @@
-import { shortenLargeNumber } from "../utils.js"
+import { shortenLargeNumber, fmtMSS } from "../utils.js"
 import Component from './component.js';
 import store from '../store/index.js';
 
@@ -13,12 +13,17 @@ export default class Dashboard extends Component {
 	}
 
 	getData() {
-		return nimo.call('dashboard')
+		return nimo.call('dashboard', {
+			body: {
+				from_date: store.state.filter[0].format('YYYY-MM-DD HH:mm:ss')
+			}
+		})
 	}
 
 	refresh() {
 		this.getData().then(data => {
 			this.data = data;
+			this.data.averageTime = fmtMSS(this.data.averageTime)
 			this.render();
 		});
 	}
@@ -28,41 +33,41 @@ export default class Dashboard extends Component {
 			<div class="card rounded py-2 px-4">
 				<div class="label text-xs text-gray-800">Unique Visitors</div>
 				<div class="flex items-end justify-between mt-1">
-					<span id="uniqueVisitors" class="font-semibold text-2xl -mb-1">3.6 K</span>
+					<span id="unique" class="font-semibold text-2xl -mb-1"></span>
 					<span class="text-xs mb-1"><span class="text-green-500 font-bold">↑</span> 13%</span>
 				</div>
 			</div>
 			<div class="card rounded py-2 px-4">
 				<div class="label text-xs text-gray-800">Total Page Views</div>
 				<div class="flex items-end justify-between mt-1">
-					<span id="pageViews" class="font-semibold text-2xl -mb-1">23.4 K</span>
+					<span id="total" class="font-semibold text-2xl -mb-1"></span>
 					<span class="text-xs mb-1"><span class="text-green-500 font-bold">↑</span> 19%</span>
 				</div>
 			</div>
 			<div class="card rounded py-2 px-4">
 				<div class="label text-xs text-gray-800">Avergae Time on Site</div>
 				<div class="flex items-end justify-between mt-1">
-					<span id="averageTimeOnSite" class="font-semibold text-2xl -mb-1">05:24</span>
+					<span id="averageTime" class="font-semibold text-2xl -mb-1"></span>
 					<span class="text-xs mb-1"><span class="text-red-500 font-bold">↓</span> 12%</span>
 				</div>
 			</div>
 			<div class="card rounded py-2 px-4">
 				<div class="label text-xs text-gray-800">Bounce Rate</div>
 				<div class="flex items-end justify-between mt-1">
-					<span id="bouncRate" class="font-semibold text-2xl -mb-1">68%</span>
+					<span id="bounce" class="font-semibold text-2xl -mb-1"></span>
 				</div>
 			</div>
 		</div>`);
 
-		this.dashboard.append('#statsDashboard');
-
-		this.stats.uniqueVisitors = this.dashboard.find('#uniqueVisitors');
-		this.stats.pageViews = this.dashboard.find('#pageViews');
-		this.stats.averageTimeOnSite = this.dashboard.find('#averageTimeOnSite');
-		this.stats.bouncRate = this.dashboard.find('#bouncRate');
+		this.stats.unique = this.dashboard.find('#unique');
+		this.stats.total = this.dashboard.find('#total');
+		this.stats.averageTime = this.dashboard.find('#averageTime');
+		this.stats.bounce = this.dashboard.find('#bounce');
 	}
 
 	render() {
+		this.dashboard.append('#statsDashboard');
+
 		Object.keys(this.data).forEach(key => {
 			let value = this.data[key];
 			if (typeof value === "number") {
